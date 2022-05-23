32cb93d48edffb266fed59cb9001315ea94fc5d2
OmarArias <omarxarias@gmail.com>
2022-05-22 21:22:31 -0400

    Pruebas de mazo, y algunas variantes de solitario

--- test_pruebas.py
+++ test_pruebas.py
@@ -1,6 +1,10 @@
 import pytest
 from main import *
 from carta import *
+from mazo import *
+from solitario_thumbandpouch import *
+from solitario_ejemplo import *
+from solitariocatorce import *
 import carta
 import main
 from io import StringIO
@@ -362,4 +366,229 @@ def test_main_four(monkeypatch):
 #     assert double() == 2468
 
 
- 
+'''Pruebas de mazo.py'''
+
+def test_mazo_crearMazo():
+    crear_mazo()
+    assert 1
+
+def test_mazo_crearMazo2():
+    crear_mazo(1,1)
+    assert 1
+
+def test_mazo_crearMazo3():
+    crear_mazo(1,2)
+    assert 1
+    
+def test_mazo_mazoIndividual():
+    mazo = PilaCartas()
+    mazo_individual(mazo, 3)
+    assert 1
+
+def test_mazo_condMismoColor_T():
+    assert cond_mismo_color(0, 3) == True
+    
+def test_mazo_condMismoColor_T2():
+    assert cond_mismo_color(1, 2) == True
+    
+def test_mazo_condMismoColor_F():
+    assert cond_mismo_color(0, 2) == False
+
+'''Pruebas de solitario_thumbandpouch.py'''
+
+def test_thumbandpounch_armar():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    assert 1
+
+def test_thumbandpounch_termino_T():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    assert solit.termino() == True
+
+
+def test_thumbandpounch_termino_F():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    assert solit.termino() == False
+
+def test_thumbandpounch_jugar():
+    mesa = Mesa()
+    jugada = mesa.parsear_jugada('m')
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    solit.jugar(jugada)
+    assert 1
+
+def test_thumbandpounch_cartaAPila_Error():
+    pilaA = PilaCartas()
+    pilaB = PilaCartas()
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    with pytest.raises(SolitarioError):
+        solit._carta_a_pila(pilaA, pilaB)
+        
+def test_thumbandpounch_cartaAPila():
+    pilaA = PilaCartas()
+    pilaB = PilaCartas()
+    cartaA = Carta(2,2,True)
+    cartaB = Carta(3,2,False)
+    mesa = Mesa()
+    mesa.pilas_tablero.append(pilaA)
+    mesa.pilas_tablero.append(pilaB)
+    solit = SolitarioThumbAndPouch(mesa)
+    pilaA.apilar(cartaA)
+    pilaA.apilar(cartaB)
+    solit._carta_a_pila(pilaA, pilaB)
+    assert 1
+    
+def test_thumbandpounch_subpilaAPila_Error():
+    pilaA = PilaCartas()
+    pilaB = PilaCartas()
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    with pytest.raises(SolitarioError):
+        solit._subpila_a_pila(pilaA, pilaB)
+        
+def test_thumbandpounch_renovar():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    jugada = mesa.parsear_jugada('m')
+    solit.jugar(jugada)
+    solit.renovar_mazo()
+    assert 1
+
+def test_thumbandpounch_auxJugada_Error():
+    mesa = Mesa()
+    jugada = mesa.parsear_jugada('q')
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    j0, p0 = jugada[0]
+    j1, p1 = jugada[1] if len(jugada) == 2 else (SALIR, 0)
+    with pytest.raises(SolitarioError):
+        solit.auxiliar_jugar(len(jugada), j0, p0, j1, p1)
+
+def test_thumbandpounch_auxJugada_Error2():
+    mesa = Mesa()
+    pila = PilaCartas()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    jugada = mesa.parsear_jugada('n')
+    with pytest.raises(SolitarioError):
+        solit.jugar(jugada)
+        
+def test_thumbandpounch_auxJugada_Error3():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    jugada = mesa.parsear_jugada('m')
+    pila = PilaCartas()
+    solit.mesa.mazo = pila
+    with pytest.raises(SolitarioError):
+        solit.auxiliar_jugar(1,0,1,0,1)
+        
+def test_thumbandpounch_auxJugada():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    jugada = mesa.parsear_jugada('m')
+    solit.auxiliar_jugar(2, 1,0,PILA_TABLERO,0)
+    assert 1
+    
+def test_thumbandpounch_auxJugada_Error4():
+    mesa = Mesa()
+    solit = SolitarioThumbAndPouch(mesa)
+    solit.armar()
+    jugada = mesa.parsear_jugada('m')
+    with pytest.raises(SolitarioError):
+        solit.auxiliar_jugar(2, 1,1,PILA_TABLERO,1)
+
+
+'''Pruebas de solitario_ejemplo.py'''
+
+def test_solitEjem_armar():
+    mesa = Mesa()
+    solit = SolitarioEjemplo(mesa)
+    solit.armar()
+    assert 1
+    
+def test_solitEjem_Termino_T():
+    mesa = Mesa()
+    solit = SolitarioEjemplo(mesa)
+    assert solit.termino() == True
+    
+def test_solitEjem_Termino_F():
+    mesa = Mesa()
+    solit = SolitarioEjemplo(mesa)
+    solit.armar()
+    assert solit.termino() == False
+
+def test_solitEjem_jugar_Error():
+    mesa = Mesa()
+    jugada = mesa.parsear_jugada('m')
+    solit = SolitarioEjemplo(mesa)
+    solit.armar()
+    with pytest.raises(SolitarioError):
+        solit.jugar(jugada)
+
+def test_solitEjem_jugar():
+    mesa = Mesa()
+    solit = SolitarioEjemplo(mesa)
+    solit.armar()
+    jugada2 = [(PILA_TABLERO,0),(PILA_TABLERO, 1)]
+    jugada = mesa.parsear_jugada('1')
+    print(jugada2)
+    solit.jugar(jugada2)
+    assert 1
+
+def test_solitEjem_jugar2():
+    mesa = Mesa()
+    solit = SolitarioEjemplo(mesa)
+    solit.armar()
+    jugada2 = [(PILA_TABLERO,0)]
+    print(jugada2)
+    solit.jugar(jugada2)
+    assert 1
+    
+'''Pruebas de solitario_catorce.py'''
+
+def test_solitCatorce_armar():
+    mesa = Mesa()
+    solit = SolitarioCatorce(mesa)
+    solit.armar()
+    assert 1
+    
+def test_solitCatorce_Termino_T():
+    mesa = Mesa()
+    solit = SolitarioCatorce(mesa)
+    assert solit.termino() == True
+    
+def test_solitCatorce_Termino_F():
+    mesa = Mesa()
+    solit = SolitarioCatorce(mesa)
+    solit.armar()
+    assert solit.termino() == False
+    
+def test_solitCatorce_jugar_Error():
+    mesa = Mesa()
+    jugada = mesa.parsear_jugada('m')
+    solit = SolitarioCatorce(mesa)
+    solit.armar()
+    with pytest.raises(SolitarioError):
+        solit.jugar(jugada)
+
+def test_solitCatorce_jugar():
+    mesa = Mesa()
+    solit = SolitarioCatorce(mesa)
+    solit.armar()
+    jugada2 = [(PILA_TABLERO,0),(PILA_TABLERO, 1)]
+    jugada = mesa.parsear_jugada('1')
+    cartaA = Carta(6,2,False)
+    cartaB = Carta(8,1,False)
+    solit.mesa.pilas_tablero[0].apilar(cartaA, True)
+    solit.mesa.pilas_tablero[1].apilar(cartaB, True)
+    solit.jugar(jugada2)
+    assert 1 
